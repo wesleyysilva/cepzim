b91df8bf259ad53b8b075788bfd566df5ec75888
Wesley Silva <wesleyysilva@yahoo.com.br>
2022-10-13 11:01:46 -0300

    update isOK.ts

--- src/isOK.ts
+++ src/isOK.ts
@@ -1,19 +1,32 @@
 import { ViaCEP } from "./services/ViaCEP";
+import { CorreiosCEP} from "./services/CorreiosCEP"
 
-
-async function getCEP(cep: string){
+async function getCEP_ViaCEP(cep: string){
    const viacep = new ViaCEP();
-   console.log(await viacep.findByCEP(cep));
+   const data = await viacep.findByCEP(cep); 
+
+   console.log(data);
 }
 
-async function get_On_Street(uf: string, city: string, street: string){
+async function get_On_Street_ViaCEP(uf: string, city: string, street: string){
     const viacep = new ViaCEP();
-    console.log(await viacep.findByStreet(uf, city, street));
+    const data = await viacep.findByStreet(uf, city, street);
+
+    console.log(data);
  }
- 
+
+ async function getCEP_Correios(cep: string){
+   const correioscep = new CorreiosCEP();
+   const data = await correioscep.findByCEP(cep);
+
+   console.log(data);
+}
 
 
-get_On_Street('MG','Belo Horizonte', 'Rio Branco')
-getCEP('36030600')
+get_On_Street_ViaCEP('MG','Belo Horizonte', 'Rio Branco')
+
+getCEP_ViaCEP('36030600')
+
+getCEP_Correios('36030600')
 
 
